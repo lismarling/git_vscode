04ed446bcda8b0756fc25678e9b75836cc341478
“lisaleman“ <lisaleman@gmail.com>
2023-08-31 13:26:39 -0600

    Acabo de agregar un div con anuncio

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <body>
+<div class="sidenav">
+         <div class="login-main-text">
+            <h2>Hola desde git<br></h2>
+        
+                
+        </a>
+            
+         </div>
 
 <div class="sidenav">
          <div class="login-main-text">
